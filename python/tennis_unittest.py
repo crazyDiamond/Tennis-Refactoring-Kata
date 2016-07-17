f12022f52b6e4f158643ffe397e756e752f31e47
Jeeva Nadarajah <jeeva.ganeshan@gmail.com>
2016-07-17 15:28:18 -0400

    starting refactor

--- python/tennis_unittest.py
+++ python/tennis_unittest.py
@@ -55,9 +55,9 @@ def play_game(TennisGame, p1Points, p2Points, p1Name, p2Name):
     game = TennisGame(p1Name, p2Name)
     for i in range(max(p1Points, p2Points)):
         if i < p1Points:
-            game.won_point(p1Name)
+            game.add_point_for_player(p1Name)
         if i < p2Points:
-            game.won_point(p2Name)
+            game.add_point_for_player(p2Name)
     return game
 
 class TestTennis(unittest.TestCase):
